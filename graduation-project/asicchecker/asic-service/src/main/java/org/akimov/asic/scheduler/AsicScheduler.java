101e5f3dcc6ecaa47e34ceee8a580ba4b3012229
akimov <akimov@sprut.ru>
2023-11-05 18:52:11 +0300

    ref

--- graduation-project/asicchecker/asic-service/src/main/java/org/akimov/asic/scheduler/AsicScheduler.java
+++ graduation-project/asicchecker/asic-service/src/main/java/org/akimov/asic/scheduler/AsicScheduler.java
@@ -1,17 +1,13 @@
 package org.akimov.asic.scheduler;
 
-//import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.MeterRegistry;
-import lombok.RequiredArgsConstructor;
 import org.akimov.asic.service.AsicService;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
-//@RequiredArgsConstructor
 public class AsicScheduler {
      private final AsicService asicService;
     private final AtomicInteger averageHashrate;
@@ -25,17 +21,10 @@ public class AsicScheduler {
         meterRegistry.gauge("maxChipTemp", maxChipTemp);
     }
 
-    @Async
     @Scheduled(fixedRate = 10_000)
-    public void updateAverageHashrate() {
+    public void updateMainParameters() {
         var summary = asicService.getSummary();
         averageHashrate.set((int) summary.getMiner().getAverageHashrate());
-    }
-
-    @Async
-    @Scheduled(fixedRate = 10_000)
-    public void updateMaxChipTemp() {
-        var summary = asicService.getSummary();
         maxChipTemp.set(summary.getMiner().getChipTemp().getMax());
     }
 }
