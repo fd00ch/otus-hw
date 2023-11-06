9627b6cc6923b8271026807fb5db14a357453889
akimov <akimov@sprut.ru>
2023-11-07 02:55:46 +0300

    Scheduling config

--- graduation-project/asicchecker/asic-service/src/main/java/org/akimov/asic/AsicServiceApplication.java
+++ graduation-project/asicchecker/asic-service/src/main/java/org/akimov/asic/AsicServiceApplication.java
@@ -2,9 +2,7 @@ package org.akimov.asic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class AsicServiceApplication {
     public static void main(String[] args) {
---
+++ graduation-project/asicchecker/asic-service/src/main/java/org/akimov/asic/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package org.akimov.asic.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
