0528fd37fe99d3086c9f6a1c70d200bd710567b5
akimov <akimov@sprut.ru>
2023-11-07 03:08:58 +0300

    remove unnecessary

--- graduation-project/asicchecker/telegram-service/src/main/java/org/akimov/intergation/AsicServiceClient.java
+++
@@ -1,9 +0,0 @@
-package org.akimov.intergation;
-
-import org.akimov.model.status.Status;
-import org.akimov.model.summary.Summary;
-
-public interface AsicServiceClient {
-    Status getAsicStatus();
-    Summary getAsicSummary();
-}
