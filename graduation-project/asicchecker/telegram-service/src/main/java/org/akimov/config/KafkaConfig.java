2ac04f4fd97ad7722e72ff850cdd794a32e4d1dc
akimov <akimov@sprut.ru>
2023-11-08 03:08:41 +0300

    kafka listener for sending telegram messages

---
+++ graduation-project/asicchecker/telegram-service/src/main/java/org/akimov/config/KafkaConfig.java
@@ -0,0 +1,47 @@
+package org.akimov.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.akimov.exception.BotException;
+import org.akimov.model.telegram.TelegramMessage;
+import org.akimov.service.TelegramBot;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+@EnableKafka
+public class KafkaConfig {
+
+    public static final String SEND_TELEGRAM_MSG = "send-telegram-msg";
+    public static final String GROUP_ID = "telegram-service";
+    private final TelegramBot telegramBot;
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public NewTopic sendTelegramMessageTopic() {
+        return TopicBuilder
+                .name(SEND_TELEGRAM_MSG)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @KafkaListener(groupId = GROUP_ID, topics = SEND_TELEGRAM_MSG)
+    public void asicServiceResponseListen(String msgAsString) {
+        TelegramMessage telegramMessage;
+        try {
+            telegramMessage = objectMapper.readValue(msgAsString, TelegramMessage.class);
+        } catch (Exception ex) {
+            log.error("can't parse message:{}", msgAsString, ex);
+            throw new BotException("can't parse message:" + msgAsString, ex);
+        }
+        telegramBot.sendTelegramMsg(telegramMessage.getChatId(), telegramMessage.getMessage());
+    }
+}
