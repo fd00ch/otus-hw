9f9e7b81f9f2db1e4fac25765570c91aeaee39aa
akimov <akimov@sprut.ru>
2023-11-09 23:06:00 +0300

    Asic integration interface

--- graduation-project/asicchecker/asic-service/src/main/java/org/akimov/asic/integrations/AsicIntegration.java
+++ graduation-project/asicchecker/asic-service/src/main/java/org/akimov/asic/integrations/AsicIntegration.java
@@ -1,55 +1,10 @@
 package org.akimov.asic.integrations;
 
-import lombok.RequiredArgsConstructor;
 import org.akimov.asic.model.status.Status;
 import org.akimov.asic.model.summary.Summary;
-import org.akimov.asic.model.token.AdminTokenRequest;
-import org.akimov.asic.model.token.AdminTokenResponse;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
 
-@Component
-@RequiredArgsConstructor
-public class AsicIntegration {
-    private final WebClient asicServiceWebClient;
-
-    public Status getStatus() {
-        return asicServiceWebClient.get()
-                .uri("/api/v1/status")
-                .retrieve()
-                .bodyToMono(Status.class)
-                .block();
-    }
-
-    public Summary getSummary() {
-        return asicServiceWebClient.get()
-                .uri("/api/v1/summary")
-                .retrieve()
-                .bodyToMono(Summary.class)
-                .block();
-    }
-
-    public void reboot() {
-        var adminToken = getAdminToken();
-        asicServiceWebClient.post()
-                .uri("/api/v1/system/reboot")
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("Authorization", "Bearer " + adminToken)
-                .retrieve()
-                .toBodilessEntity()
-                .block();
-    }
-
-    private String getAdminToken() {
-        var adminTokenRequest = new AdminTokenRequest("admin");
-        var answer = asicServiceWebClient.post()
-                .uri("/api/v1/unlock")
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(adminTokenRequest)
-                .retrieve()
-                .bodyToMono(AdminTokenResponse.class)
-                .block();
-        return answer.getToken();
-    }
+public interface AsicIntegration {
+    Status getStatus();
+    Summary getSummary();
+    void reboot();
 }
