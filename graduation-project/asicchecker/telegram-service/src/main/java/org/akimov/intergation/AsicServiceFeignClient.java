6dda7c16b893359521845fbae22fa36378d1b50a
akimov <akimov@sprut.ru>
2023-11-09 23:09:37 +0300

    Reboot command

--- graduation-project/asicchecker/telegram-service/src/main/java/org/akimov/intergation/AsicServiceFeignClient.java
+++ graduation-project/asicchecker/telegram-service/src/main/java/org/akimov/intergation/AsicServiceFeignClient.java
@@ -4,6 +4,7 @@ import org.akimov.model.status.Status;
 import org.akimov.model.summary.Summary;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @FeignClient(name = "asic-service-client", url = "http://192.168.1.254:8071")
@@ -13,4 +14,7 @@ public interface AsicServiceFeignClient {
 
     @GetMapping("/api/v1/asic/summary")
     Summary getAsicSummary();
+
+    @PostMapping("/api/v1/asic/reboot")
+    Summary reboot();
 }
--- graduation-project/asicchecker/telegram-service/src/main/java/org/akimov/service/processors/CmdRegistry.java
+++ graduation-project/asicchecker/telegram-service/src/main/java/org/akimov/service/processors/CmdRegistry.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum CmdRegistry {
+    // get info
     HR("/hr", "messageProcessorHashRate"),
     TEMP("/temp", "messageProcessorTemp"),
     PWR("/pwr", "messageProcessorPower"),
@@ -11,7 +12,10 @@ public enum CmdRegistry {
     PCB_HR("/pcbhr", "messageProcessorPcbHr"),
     PCB_TEMP("/pcbtemp", "messageProcessorPcbTemp"),
     PCB_PWR("/pcbpwr", "messageProcessorPcbPwr"),
-    PCB_VOLT("/pcbvolt", "messageProcessorPcbVolt");
+    PCB_VOLT("/pcbvolt", "messageProcessorPcbVolt"),
+
+    // control
+    REBOOT("/reboot", "messageProcessorReboot");
 
     private final String cmd;
     private final String handlerName;
