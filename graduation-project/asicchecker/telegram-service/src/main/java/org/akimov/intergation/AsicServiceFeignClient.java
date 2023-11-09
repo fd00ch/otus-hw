167dcd355a6c407b372497830b992a3c4384e9aa
akimov <akimov@sprut.ru>
2023-11-09 22:58:46 +0300

    feign client refactoring

--- graduation-project/asicchecker/telegram-service/src/main/java/org/akimov/intergation/AsicServiceFeignClient.java
+++ graduation-project/asicchecker/telegram-service/src/main/java/org/akimov/intergation/AsicServiceFeignClient.java
@@ -2,16 +2,15 @@ package org.akimov.intergation;
 
 import org.akimov.model.status.Status;
 import org.akimov.model.summary.Summary;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.cloud.openfeign.FeignClient;
 
 
 @FeignClient(name = "asic-service-client", url = "http://192.168.1.254:8071")
 public interface AsicServiceFeignClient {
-    @RequestMapping(method = RequestMethod.GET, value = "/api/v1/asic/status")
+    @GetMapping("/api/v1/asic/status")
     Status getAsicStatus();
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/v1/asic/summary")
+    @GetMapping("/api/v1/asic/summary")
     Summary getAsicSummary();
 }
