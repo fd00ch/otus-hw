bdbc845e9c561d297069d03cbffb0938cf36c95b
akimov <akimov@sprut.ru>
2023-11-12 01:42:13 +0300

    test container tests

--- hw17-spring-data-jdbc-test-container/L28-springDataJdbc/build.gradle.kts
+++ hw17-spring-data-jdbc-test-container/L28-springDataJdbc/build.gradle.kts
@@ -6,5 +6,13 @@ dependencies {
         implementation ("com.google.code.findbugs:jsr305")
 
         implementation ("org.springframework.boot:spring-boot-starter-data-jdbc")
+        testImplementation("com.h2database:h2")
+        testImplementation("org.junit.jupiter:junit-jupiter-engine")
+        testImplementation("org.junit.jupiter:junit-jupiter-params")
+        testImplementation("org.assertj:assertj-core")
+
+        testImplementation("org.testcontainers:junit-jupiter")
+        testImplementation("org.testcontainers:postgresql")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
     }
 }
